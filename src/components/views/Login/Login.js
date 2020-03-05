2985afea82112a90f699f16ad821756bc4dabad9
sinsys <nicholas.hazel@gmail.com>
2020-03-05 00:04:11 -0800

    Imported scaffolding for login

---
+++ src/components/views/Login/Login.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './Login.scss';
+
+class Login extends Component {
+
+  state = {
+
+  };
+
+  render() {
+    return (
+      <section className="Login">
+        <h1>
+          Login
+        </h1>
+      </section>
+    );
+  };
+
+};
+
+export default Login;
---
+++ src/components/views/Signup/Signup.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './Signup.scss';
+
+class Signup extends Component {
+
+  state = {
+
+  };
+
+  render() {
+    return (
+      <section className="Signup">
+        <h1>
+          Signup
+        </h1>
+      </section>
+    );
+  };
+
+};
+
+export default Signup;
