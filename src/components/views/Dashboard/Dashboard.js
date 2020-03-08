f57ad92bc1c978fafcf46bbc0c2e686b05b88a26
sinsys <nicholas.hazel@gmail.com>
2020-03-07 20:51:21 -0800

    Basic styling on task list for dashboard

--- src/components/views/Dashboard/Dashboard.js
+++ src/components/views/Dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import { UserContext } from 'contexts/UserContext';
 
 import Timer from 'components/widgets/Timer/Timer';
 import HydrationGauge from 'components/widgets/HydrationGauge/HydrationGauge';
-
+import Button from 'components/elements/Button/Button';
 import './Dashboard.scss';
 
 const Dashboard = () => {
@@ -18,6 +18,34 @@ const Dashboard = () => {
       <div className="start-work_wrapper"></div>
       <Timer />
       <HydrationGauge percent={25}/>
+
+      <div class="Tasks-list_wrapper">
+        <h2>Tasks</h2>
+        <div class="Tasks-list">
+          <div class="Task">
+            <p>This is a task title</p>
+            <span>1d 2hr 48m</span>
+          </div>
+          <div class="Task">
+            <p>This is a task title</p>
+            <span>1d 2hr 48m</span>
+          </div>
+          <div class="Task">
+            <p>This is a task title</p>
+            <span>1d 2hr 48m</span>
+          </div>
+        </div>
+        <div class="Tasks-view-all-btn">
+          <Button
+            id="view-all"
+            className="view-all-btn"
+            type="button"
+            name="view-all"
+            text="View all"
+          />
+        </div>
+      </div>
+
     </div>
     
   );
