5f9f8054f7f3e518bcca3e860e5be432202cf308
sinsys <nicholas.hazel@gmail.com>
2020-03-19 00:10:11 -0700

    Updated endpoint for production URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://intense-reef-86007.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
