16414ddd3968ce571c4be43e8d56b23f47d2a471
sinsys <nicholas.hazel@gmail.com>
2020-03-19 01:26:35 -0700

    Setting up endpoints and secrets for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // API_ENDPOINT: 'http://localhost:8000/api',
-  API_ENDPOINT: 'https://intense-reef-86007.herokuapp.com/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'blogful-client-auth-token',
+  API_BASE_URL: process.env.REACT_APP_API_ENDPOINT ||
+  "http://localhost:8000/api"
 }
