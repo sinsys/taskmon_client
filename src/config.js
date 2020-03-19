4050d2a9b064f896eaae9864216e10919110045d
sinsys <nicholas.hazel@gmail.com>
2020-03-19 01:35:34 -0700

    Setting up endpoints and secrets for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'blogful-client-auth-token',
-  API_BASE_URL: process.env.REACT_APP_API_ENDPOINT ||
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT ||
   "http://localhost:8000/api"
 }
