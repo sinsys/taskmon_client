cf7dde3685c41c085f2029a46bd51fcbfcde7805
sinsys <nicholas.hazel@gmail.com>
2020-03-19 17:12:06 -0700

    User registration functionality complete.

--- src/components/views/Signup/Signup.test.js
+++ src/components/views/Signup/Signup.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { UserContextProvider } from 'contexts/UserContext';
 import Signup from './Signup';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
   	<Router>
-  		<Signup />
+      <UserContextProvider>
+        <Signup />
+      </UserContextProvider>
   	</Router>,
   	div
   );
